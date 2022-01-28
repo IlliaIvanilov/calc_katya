d8a59886e5487aadc71a0be1e1f678aadce6f6ae
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-01-28 15:20:58 +0200

    change logic progmams

--- culc_katya/culc_katya/Form1.Designer.cs
+++ culc_katya/culc_katya/Form1.Designer.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Drawing;
-using System.Threading;
 using System.Windows.Forms;
-namespace culc_katya
+
+namespace main_k_calc
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -27,100 +27,210 @@ namespace culc_katya
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
+        static double length = 0;
+        static double avFuelConsumption = 0;
+        static double price = 0;
         private void InitializeComponent()
         {
+
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 600);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Онлайн калькулятор";
+            this.BackColor = System.Drawing.Color.FromArgb(241, 212, 212);
+            this.Size = new System.Drawing.Size(400, 600);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MinimumSize = this.MaximumSize = this.Size;
+            this.StartPosition = FormStartPosition.CenterParent;
+            //this.Icon = new Icon("logo.ico");
 
-            Label name_label = new Label();
-            name_label.Text = "Калькулятор по расходу топлива";
-            name_label.Location = new Point(100, 10);
-            name_label.AutoSize = true;
-            this.Controls.Add(name_label);
+            color = new Label();
+            this.color.Location = new System.Drawing.Point(0, 0);
+            this.color.Size = new System.Drawing.Size(400, 45);
+            this.color.BackColor = System.Drawing.Color.FromArgb(172, 141, 175);
+            Controls.Add(color);
 
-            Label km_label = new Label();
-            km_label.Text = "Сколько хотите проехать(км)";
-            km_label.Location = new Point(20, 50);
-            km_label.AutoSize = true;
-            this.Controls.Add(km_label);
+            Text_online_label = new Label();
+            this.Text_online_label.Text = "Онлайн калькулятор \n  по расходу топлива";
+            this.Text_online_label.Font = new Font("Times New Roman", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+            this.Text_online_label.ForeColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.Text_online_label.Location = new System.Drawing.Point(90, 55);
+            this.Text_online_label.Size = new System.Drawing.Size(400, 45);
+            Controls.Add(Text_online_label);
 
-            TextBox km_textbox = new TextBox();
-            km_textbox.Location = new Point(km_label.Location.X + 250, km_label.Location.Y);
-            km_textbox.AutoSize = true;
-            this.Controls.Add(km_textbox);
+            how_label = new Label();
+            this.how_label.Text = "Сколько хотите проехать \n(км)";
+            this.how_label.Location = new System.Drawing.Point(20, 120);
+            this.how_label.Size = new System.Drawing.Size(200, 50);
+            this.how_label.ForeColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.how_label.Font = new Font("Times New Roman", 15, FontStyle.Bold, GraphicsUnit.Pixel);
+            Controls.Add(how_label);
 
-            Label l_km_label = new Label();
-            l_km_label.Text = "Средний расход топлива(л/100км)";
-            l_km_label.Location = new Point(km_label.Location.X, km_label.Location.Y + 50);
-            l_km_label.AutoSize = true;
-            this.Controls.Add(l_km_label);
+            how_TextBox = new TextBox();
+            how_TextBox.Location = new System.Drawing.Point(250, 125);
+            how_TextBox.TextChanged += UserName_TextBox_TextChanged;
+            this.Controls.Add(how_TextBox);
 
-            TextBox l_km_textbox = new TextBox();
-            l_km_textbox.Location = new Point(l_km_label.Location.X + 250, l_km_label.Location.Y);
-            km_textbox.AutoSize = true;
-            this.Controls.Add(l_km_textbox);
+            average_label = new Label();
+            this.average_label.Text = "Средний расход топлива \n(л /100 км)";
+            this.average_label.Location = new System.Drawing.Point(20, 185);
+            this.average_label.Size = new System.Drawing.Size(200, 50);
+            this.average_label.ForeColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.average_label.Font = new Font("Times New Roman", 15, FontStyle.Bold, GraphicsUnit.Pixel);
+            Controls.Add(average_label);
 
-            Label cost_label = new Label();
-            cost_label.Text = "Стоимость 1 л. топлива(грн)";
-            cost_label.Location = new Point(l_km_label.Location.X, l_km_label.Location.Y + 50);
-            cost_label.AutoSize = true;
-            this.Controls.Add (cost_label);
+            average_TextBox = new TextBox();
+            average_TextBox.Location = new System.Drawing.Point(250, 190);
+            average_TextBox.TextChanged += UserName_TextBox_TextChanged;
+            this.Controls.Add(average_TextBox);
 
-            TextBox cost_textbox = new TextBox();
-            cost_textbox.Location = new Point (cost_label.Location.X + 250, cost_label.Location.Y);
-            cost_textbox.AutoSize = true;
-            this.Controls.Add (cost_textbox);
+            price_label = new Label();
+            this.price_label.Text = "Стоимость 1л топлива \n(руб)";
+            this.price_label.Location = new System.Drawing.Point(20, 250);
+            this.price_label.Size = new System.Drawing.Size(200, 50);
+            this.price_label.ForeColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.price_label.Font = new Font("Times New Roman", 15, FontStyle.Bold, GraphicsUnit.Pixel);
+            Controls.Add(price_label);
 
-            this.res_btn = new Button();
-            res_btn.Location = new Point(cost_label.Location.X + 30, cost_label.Location.Y + 50);
-            res_btn.Size = new Size(300, 50);
-            res_btn.Text = "Рассчитать";
-            this.Controls.Add(res_btn);
+            price_TextBox = new TextBox();
+            price_TextBox.Location = new System.Drawing.Point(250, 255);
+            price_TextBox.TextChanged += UserName_TextBox_TextChanged;
+            this.Controls.Add(price_TextBox);
 
-            Label need_label = new Label();
-            need_label.Text = "Потребуется топлива";
-            need_label.Location = new Point(cost_label.Location.X, cost_label.Location.Y + 150);
-            need_label.AutoSize = true;
-            this.Controls.Add(need_label);
+            this.button.Location = new System.Drawing.Point(40, 300);
+            this.button.Text = "Рассчитать";
+            this.button.Enabled = false;
+            this.button.BackColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.button.ForeColor = System.Drawing.Color.FromArgb(241, 212, 212);
+            this.button.Font = new Font("Times New Roman", 18, FontStyle.Bold, GraphicsUnit.Pixel);
+            this.button.Size = new Size(150, 50);
+            this.button.Click += (sender, e) =>
+            {
+                if (!double.TryParse(how_TextBox.Text, out length) ||
+                !double.TryParse(average_TextBox.Text, out avFuelConsumption) ||
+                !double.TryParse(price_TextBox.Text, out price))
+                {
+                    color2.Text = "Некорректный ввод";
+                    color2.Font = new Font("Times New Roman", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+                    color2.ForeColor = Color.Black;
+                }
+                else
+                {
+                    length = Convert.ToDouble(how_TextBox.Text);
+                    avFuelConsumption = Convert.ToDouble(average_TextBox.Text);
+                    price = Convert.ToDouble(price_TextBox.Text);
+                    if (length <= 0 || avFuelConsumption <= 0 || price <= 0)
+                    {
+                        color2.Text = "Некорректный ввод \nчисло не может быть меньше 0";
+                        color2.Font = new Font("Times New Roman", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+                        color2.ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                        double FuelNeed = ((avFuelConsumption * length) / 100);
+                        need_label2.Text = FuelNeed.ToString();
+                        double costFuel = FuelNeed * price;
+                        cost_label2.Text = costFuel.ToString();
+                    }
+                }
+            };
+            this.Controls.Add(this.button);
 
-            TextBox need_textbox = new TextBox();
-            need_textbox.Location = new Point(need_label.Location.X + 250, need_label.Location.Y);
-            need_textbox.AutoSize = true;
-            this.Controls.Add(need_textbox);
 
-            Label price_label = new Label();
-            price_label.Text = "Стоимость топлива(грн)";
-            price_label.Location = new Point(need_label.Location.X, need_label.Location.Y + 50);
-            price_label.AutoSize = true;
-            this.Controls.Add(price_label);
+            this.buttonClear.Location = new System.Drawing.Point(190, 300);
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.BackColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.buttonClear.ForeColor = System.Drawing.Color.FromArgb(241, 212, 212);
+            this.buttonClear.Font = new Font("Times New Roman", 18, FontStyle.Bold, GraphicsUnit.Pixel);
+            this.buttonClear.Size = new Size(150, 50);
+            this.buttonClear.Click += (sender, e) =>
+            {
+                price_TextBox.Clear();
+                average_TextBox.Clear();
+                how_TextBox.Clear();
+                if (color2.Text.Length != 0)
+                {
+                    color2.Text = String.Empty;
+                }
+                if (cost_label2.Text.Length != 0 || need_label2.Text.Length != 0)
+                {
+                    cost_label2.Text = String.Empty;
+                    need_label2.Text = String.Empty;
+                }
+            };
+            this.Controls.Add(this.buttonClear);
 
-            TextBox price_textbox = new TextBox();
-            price_textbox.Location = new Point(price_label.Location.X + 250, price_label.Location.Y);
-            price_textbox.AutoSize = true;
-            this.Controls.Add(price_textbox);
+
+            need_label = new Label();
+            this.need_label.Text = "Потребуется топлива \n(л)";
+            this.need_label.Location = new System.Drawing.Point(20, 375);
+            this.need_label.Size = new System.Drawing.Size(200, 50);
+            this.need_label.ForeColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.need_label.Font = new Font("Times New Roman", 15, FontStyle.Bold, GraphicsUnit.Pixel);
+            Controls.Add(need_label);
+
+
+            need_label2 = new Label();
+            this.need_label2.Text = String.Empty;
+            this.need_label2.Location = new System.Drawing.Point(250, 375);
+            this.need_label2.Size = new System.Drawing.Size(100, 25);
+            this.need_label2.BackColor = Color.White;
+            Controls.Add(need_label2);
+
+            cost_label = new Label();
+            this.cost_label.Text = "Стоимость топлива \n(руб)";
+            this.cost_label.Location = new System.Drawing.Point(20, 440);
+            this.cost_label.Size = new System.Drawing.Size(200, 50);
+            this.cost_label.ForeColor = System.Drawing.Color.FromArgb(72, 76, 127);
+            this.cost_label.Font = new Font("Times New Roman", 15, FontStyle.Bold, GraphicsUnit.Pixel);
+            Controls.Add(cost_label);
+
+            cost_label2 = new Label();
+            this.cost_label2.Text = String.Empty;
+            this.cost_label2.Location = new System.Drawing.Point(250, 440);
+            this.cost_label2.Size = new System.Drawing.Size(100, 25);
+            this.cost_label2.BackColor = Color.White;
+            Controls.Add(cost_label2);
+
+            color2 = new Label();
+            this.color2.Location = new System.Drawing.Point(0, 508);
+            this.color2.Size = new System.Drawing.Size(400, 45);
+            this.color2.BackColor = System.Drawing.Color.FromArgb(172, 141, 175);
+            Controls.Add(color2);
         }
 
-        Button res_btn;
+        private void UserName_TextBox_TextChanged(object sender, System.EventArgs e)
+        {
+            if (how_label.Text.Length >= 1 && average_TextBox.Text.Length >= 1 && price_TextBox.Text.Length >= 1)
+            {
+                button.Enabled = true;
+            }
+            else
+            {
+                button.Enabled = false;
+            }
+        }
 
-        Label name_label;
-        Label km_label;
-        Label l_km_label;
-        Label cost_label;
-        Label need_label;
+
+
+        Label Text_online_label;
+        Label color;
+        Label how_label;
+        Label average_label;
         Label price_label;
-
-        TextBox km_textbox;
-        TextBox l_km_textbox;
-        TextBox cost_textbox;
-        TextBox need_textbox;
-        TextBox price_textbox;
-
+        Label color2;
+        TextBox how_TextBox;
+        TextBox average_TextBox;
+        TextBox price_TextBox;
+        Label need_label;
+        Label cost_label;
+        Label need_label2;
+        Label cost_label2;
+        Button button = new Button();
+        Button buttonClear = new Button();
         #endregion
     }
 }
-
--- culc_katya/culc_katya/Form1.cs
+++ culc_katya/culc_katya/Form1.cs
@@ -16,17 +16,5 @@ namespace culc_katya
         {
             InitializeComponent();
         }
-        public decimal L1=0;
-        public decimal K2=0;
-        public decimal Cena=34;
-
-        private void button1_Click(object sender,EventArgs e)
-        {
-            decimal L1=NumericUpDown1.Value;
-            decimal K2=NumericUpDown2.Value;
-            textBox1.Text=Convert.ToString(L1*K2/100);
-            textBox2.Text=Convert.ToString((L1*K2/100)*Cena);
-
-        }
     }
 }
